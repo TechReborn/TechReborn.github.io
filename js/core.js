58d4178bfc81e5842472f990822b0f237c1d81d4
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2019-12-14 13:22:47 +1100

    Carousel, modlist and copyright updated

---
+++ js/core.js
@@ -0,0 +1,11 @@
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'center',
+  contain: false,
+  pageDots: false,
+  prevNextButtons: false,
+  autoPlay: true,
+  wrapAround: true,
+  pauseAutoPlayOnHover: false,
+  autoPlay: 4000
+});
